9e0ebf4aa430eb536afa7de14a53360aa013da76
D0ubleA-ron <adeo1@student.ubc.ca>
2022-12-02 20:29:54 -0800

    deleted opencsv

--- app/src/main/java/com/example/ratemypritch/DB_Helper.java
+++ app/src/main/java/com/example/ratemypritch/DB_Helper.java
@@ -7,8 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvValidationException;
+
 
 import java.io.BufferedReader;
 import java.io.InputStream;
